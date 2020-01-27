d320f4c164a0f28bc75013325186c597cbf16cdf
Jimmy_Huang <1902161621@qq.com>
2020-01-28 04:26:41 +0800

    only send snippets when source need it

---
+++ python/client/lib/event/snippets.py
@@ -0,0 +1,12 @@
+import lib.scope as scope_
+import lib.vim_or_neovim_support as vim_lib
+
+class SnippetsEvent(scope_.Event):
+    def __init__(self, source_name):
+        scope_.Event.__init__(self, source_name)
+        
+    def _pack(self, msg, event_name):
+        msg = self._basic(msg)
+        msg['Event'] = event_name
+        msg['Additional'] = self._get_snippets()
+        return msg
