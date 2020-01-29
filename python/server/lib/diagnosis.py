56b904957ef4c2b045c5601287798733435cb4cd
Jimmy_Huang <1902161621@qq.com>
2020-01-30 02:22:39 +0800

    add diagnosis for the server

---
+++ python/server/lib/diagnosis.py
@@ -0,0 +1,10 @@
+
+class Operate(object):
+    """
+    """
+    def __init__(self):
+        pass
+        
+    def Diagnosis(self, obj_,  version):
+        results = obj_.Diagnosis(version)
+        return results
